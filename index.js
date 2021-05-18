31ceed9d4772c4ab9e272144714fde5f8db09bb8
JillKlatt <klatt.jill@gmail.com>
2021-05-18 09:29:46 -0400

    commit tree 30a3d295757d555e1e4cfd76c70381680ecf698a

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 
+const record = [
+    {year: "2018", result: "L"},
+    {year: "2017", result: "W"},
+    {year: "2016", result: "N/A"}
+    //...
+  ] 
+
+  function superbowlWin(array) {
+      let solution = array.find(record => record.result === "W")
+      return !!solution ? solution.year : undefined
+      //array[result].find(function(e) { return e === "W"})
+
+  }
